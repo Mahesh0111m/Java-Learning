c653e2ba3f836adcc4308d45c15086601dd8f62e
Mahesh0111m <maheshm0111mhm@gmail.com>
2023-09-27 11:52:44 +0530

    linear search

--- Arrays.java
+++ Arrays.java
@@ -2,6 +2,25 @@ public class Arrays {
     public static void main(String[] args) {
         //Syntax
         //datatype[] variable_name = new datatype[size];
+        // store 5 roll nmbers
+
+        int[] roll = new int[5];
+
+        //for integer array by default values will be zero
+
+        System.out.println(roll[1]);
+
+        //other way
+
+        int[] roll2 = {1,23,4,5,4};
+
+        String[] roll3= new String[5];
+
+        //for string default values will be null
+
+        System.out.println(roll3[2]);
+
+
 
     }
 }
