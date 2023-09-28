6af4fd3d4010c683ac9ee72f02e82c486399d78d
Mahesh0111m <maheshm0111mhm@gmail.com>
2023-09-28 23:29:58 +0530

    linear search

--- Input.java
+++ Input.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
+        //array of primitives
+
         int[] arr=new int[5];
         System.out.println("Input for arrays");
 
@@ -37,7 +40,14 @@ public class Input {
 
 
 
+// array of objects
+        String[] str = new String[5];
+        for(int i=0;i<str.length;i++)
+        {
+            str[i]=in.next();
+        }
 
+        System.out.println(Arrays.toString(str));
 
         }
     }
