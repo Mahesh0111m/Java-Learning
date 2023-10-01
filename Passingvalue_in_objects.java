4deb9a87a3eb2bdea4b3a47a266e89290764a761
Mahesh0111m <maheshm0111mhm@gmail.com>
2023-10-01 22:33:56 +0530

    passing values in objects

---
+++ Passingvalue_in_objects.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Passingvalue_in_objects {
+    public static void main(String[] args) {
+
+        int[] arr={1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    static void change(int[] nums)
+    {
+        nums[0]=99;
+    }
+}
