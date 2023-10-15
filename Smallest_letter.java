62a71c6e76b15d894b980b2f305a7f0f0e79da8d
Mahesh0111m <maheshm0111mhm@gmail.com>
2023-10-15 14:46:05 +0530

    Smallest letter

---
+++ Smallest_letter.java
@@ -0,0 +1,31 @@
+public class Smallest_letter {
+   // https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+   public static void main(String[] args) {
+      char[] letters = {'c','f','j'};
+
+       char target = 'c';
+       char ans = nextGreatestLetter(letters,target);
+       System.out.println(ans);
+   }
+
+
+     static char nextGreatestLetter(char[] letters, char target) {
+
+        int start=0,end=letters.length-1;
+
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+            if(letters[mid] > target){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return letters[start%letters.length];
+    }
+}
+
+
+
